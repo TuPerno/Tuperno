4c6e7725fa5b050744d638679528bc616b103c17
Luciano Villanueva <lucianoivillanuevar@gmail.com>
2026-01-12 22:56:24 -0300

    Agregar prototipo visual landing v1 en docs/bosquejos

---
+++ docs/bosquejos/landing-v1/src/config/constants.ts
@@ -0,0 +1,54 @@
+// Información de contacto
+export const CONTACT_INFO = {
+  phone: '+56951997763',
+  phoneDisplay: '+56 9 5199 7763',
+  email: 'tupernorw90@gmail.com',
+  whatsappUrl: 'https://wa.me/56951997763',
+  address: {
+    street: 'Av. Pedro Aguirre Cerda, Pje. Noventa 1973',
+    city: 'San Pedro de la Paz',
+    region: 'Bío Bío',
+    postalCode: '4130010',
+    country: 'Chile'
+  },
+  coordinates: {
+    lat: -36.82,
+    lng: -73.09
+  },
+  mapsUrl: 'https://www.google.com/maps/place/Tuperno/@-36.8379669,-73.1174526,15z/data=!4m6!3m5!1s0x9669cb214d6e69f1:0x3547b3571221125f!8m2!3d-36.8379729!4d-73.1172721!16s%2Fg%2F11xt40jd1j?entry=tts'
+};
+
+// Horarios
+export const BUSINESS_HOURS = {
+  weekdays: 'Lun - Vie: 9:00 - 19:00',
+  saturday: 'Sábado: 9:00 - 14:00',
+  sunday: 'Cerrado'
+};
+
+// Redes sociales
+export const SOCIAL_MEDIA = {
+  facebook: 'https://web.facebook.com/Tuperno',
+  instagram: 'https://www.instagram.com/tuperno_90/',
+  instagramHandle: '@tuperno_90',
+  whatsappAvailable: false
+};
+
+// Colores del tema (Identidad Visual Corporativa)
+export const THEME_COLORS = {
+  // Colores Corporativos
+  black: '#121212',      // Negro
+  red: '#B71C1C',        // Rojo
+  yellow: '#FBC02D',     // Amarillo
+  
+  // Colores del Logotipo
+  blue: '#0000FE',       // Azul
+  gray: '#D8DFD8',       // Gris
+  
+  // Aliases para uso en componentes
+  primary: '#B71C1C',    // Rojo
+  primaryHover: '#8B1515',
+  secondary: '#FBC02D',  // Amarillo
+  secondaryHover: '#F9A825',
+  accent: '#0000FE',     // Azul
+  dark: '#121212'        // Negro
+};
